8f5499dbce4dbd37529462805aff11c3ca410830
Colin Stein <colinstein@me.com>
2016-02-21 09:10:53 -0500

    Make formla use Sha256 and specify version.

--- imgcat.rb
+++ imgcat.rb
@@ -3,7 +3,8 @@ require 'formula'
 class Imgcat < Formula
   homepage 'https://www.iterm2.com/images.html'
   url 'https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat'
-  sha1 'fd43c11f4a9b657382a8938907de1deb3b8fb250'
+  sha256 '036ee8aec2487a02b40d62d72918090ae022fc018589337dfd8403e08ffdd0c0'
+  version '88390cfc9b32299596a9f06d957543b6a3298c8a'
 
   def install
     bin.install 'imgcat'
