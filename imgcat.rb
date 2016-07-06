94e79a074ee02c7e1c5993fd663fa377fbc294d9
Ikuo Degawa <degawa@ikuwow.com>
2016-07-06 23:18:27 +0900

    Change sha256 to latest versions one

--- imgcat.rb
+++ imgcat.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Imgcat < Formula
   homepage 'https://www.iterm2.com/images.html'
   url 'https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat'
-  sha256 '036ee8aec2487a02b40d62d72918090ae022fc018589337dfd8403e08ffdd0c0'
+  sha256 '29228ae0190125e873cc54371604c51f41f6839eda80919e33ebc19b6b1906e6'
   version '88390cfc9b32299596a9f06d957543b6a3298c8a'
 
   def install
